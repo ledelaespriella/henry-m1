bee9f40e019c0c6f2c64c2fbe0c58909ee3659c1
ledelaespriella <luisdelaespriellaj@hotmail.com>
2022-10-29 15:01:38 -0500

    Resolviendo homeworks

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -3,20 +3,14 @@
 function BinarioADecimal(num) {
   // tu codigo aca
   const BinToDecimalManual = (num) => {
-    var temp = num.split("");
     const n = num.length;
-    var exp = [];
-    for (let i = n - 1; i >= 0; i--) {
-      exp.push(i);
-    }
-    var result = 0;
+    let result = 0;
     for (let i = 0; i < n; i++) {
-      result += parseInt(temp[i]) * (2 ** exp[i]);
+      result += num[i] * 2 ** (n - 1 - i);
     }
-
     return result;
   };
-
+  // return BinToDecimalManual(num);
   return parseInt(num, 2);
 }
 
@@ -25,20 +19,21 @@ console.log(BinarioADecimal("10101"));
 function DecimalABinario(num) {
   // tu codigo aca
   const DtoB = (num) => {
-    var bin = ""
+    let bin = "";
+    if(num === 0) return bin=0;
+    
     while (num > 0) {
-      bin =(num % 2) + bin ;
+      bin = (num % 2) + bin;
       num = Math.floor(num / 2);
     }
     return bin;
-  }
+  };
 
-  // return DtoB(num);
-  return num.toString(2);
+  return DtoB(num);
+  // return num.toString(2);
 }
 
-console.log(DecimalABinario(21));
-
+console.log(DecimalABinario(0));
 
 module.exports = {
   BinarioADecimal,
---
+++ 02-JavaScriptAvanzado-I/homework/result-homework.js
@@ -0,0 +1,155 @@
+const ejercicio1 = () => {
+  x = 1;
+  var a = 5;
+  var b = 10;
+  var c = function (a, b, c) {
+    var x = 10;
+    console.log(x);
+    console.log(a);
+    var f = function (a, b, c) {
+      b = a;
+      console.log(b);
+      b = c;
+      var x = 5;
+    };
+    f(a, b, c);
+    console.log(b);
+  };
+  c(8, 9, 10);
+  console.log(b);
+  console.log(x);
+};
+
+const ejercicio2 = () => {
+  console.log(bar);
+  console.log(baz);
+  foo();
+  function foo() {
+    console.log("Hola!");
+  }
+  var bar = 1;
+  baz = 2;
+};
+
+const ejercicio3 = () => {
+  var instructor = "Tony";
+  if (true) {
+    var instructor = "Franco";
+  }
+  console.log(instructor);
+};
+
+const ejercicio4 = () => {
+  var instructor = "Tony";
+  console.log(instructor);
+  (function () {
+    if (true) {
+      var instructor = "Franco";
+      console.log(instructor);
+    }
+  })();
+  console.log(instructor);
+};
+
+const ejercicio5 = () => {
+  var instructor = "Tony";
+  let pm = "Franco";
+  if (true) {
+    var instructor = "The Flash";
+    let pm = "Reverse Flash";
+    console.log(instructor);
+    console.log(pm);
+  }
+  console.log(instructor);
+  console.log(pm);
+};
+
+const ejercicio6 = () => {
+  console.log(6 / "3");
+  console.log("2" * "3");
+  console.log(4 + 5 + "px");
+  console.log("$" + 4 + 5);
+  console.log("4" - 2);
+  console.log("4px" - 2);
+  console.log(7 / 0);
+  console.log({}[0]);
+  console.log(parseInt("09"));
+  console.log(5 && 2);
+  console.log(2 && 5);
+  console.log(5 || 0);
+  console.log(0 || 5);
+  console.log([3] + [3] - [10]);
+  console.log(3 > 2 > 1);
+  console.log([] == ![]);
+};
+
+const ejercicio7 = () => {
+  function test() {
+    console.log(a);
+    console.log(foo());
+
+    var a = 1;
+    function foo() {
+      return 2;
+    }
+  }
+  test();
+};
+
+const ejercicio8 = () => {
+  var snack = "Meow Mix";
+
+  function getFood(food) {
+    if (food) {
+      var snack = "Friskies";
+      return snack;
+    }
+    return snack;
+  }
+
+  getFood(false);
+};
+
+const ejercicio9 = () => {
+  var fullname = "Juan Perez";
+  var obj = {
+    fullname: "Natalia Nerea",
+    prop: {
+      fullname: "Aurelio De Rosa",
+      getFullname: function () {
+        return this.fullname;
+      },
+    },
+  };
+
+  console.log(obj.prop.getFullname());
+
+  var test = obj.prop.getFullname;
+
+  console.log(test());
+};
+
+const ejercicio10 = () => {
+  function printing() {
+    console.log(1);
+    setTimeout(function () {
+      console.log(2);
+    }, 1000);
+    setTimeout(function () {
+      console.log(3);
+    }, 0);
+    console.log(4);
+  }
+
+  printing();
+};
+//ejercicio1();
+// ejercicio2();
+// ejercicio3();
+// ejercicio4();
+// ejercicio5();
+// ejercicio6();
+// ejercicio7();
+// ejercicio8();
+// ejercicio9();
+// ejercicio10();
